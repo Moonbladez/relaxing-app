e71e79bb7ec4981dcd4eb6f58fd734d7aa6a9bfc
Bekki Young <moonbladez@hotmail.com>
2020-04-27 20:51:47 +0200

    js added for transforms

---
+++ app.js
@@ -0,0 +1,28 @@
+//dom elements
+const container = document.querySelector("#container")
+const text = document.querySelector(".text")
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5
+
+const breathAnimation = () => {
+
+    text.innerText = "Breath in"
+    container.className = "container grow"
+
+    setTimeout(() => {
+        text.innerText = "Hold"
+
+        setTimeout(() => {
+            text.innerText = "and breath out"
+            container.className = "container shrink"
+        }, holdTime)
+    }, breatheTime)
+}
+
+setInterval(breathAnimation, totalTime)
+
+
+
+breathAnimation()
